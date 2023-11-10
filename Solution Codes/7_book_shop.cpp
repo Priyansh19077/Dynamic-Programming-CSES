8c649c5dc3fc45cc8038e91c0b40c0a3fb1b43b6
priyansh19077 <priyansh19077@iiitd.ac.in>
2023-11-10 19:05:47 +0530

    added code for 7th problem

---
+++ Solution Codes/7_book_shop.cpp
@@ -0,0 +1,47 @@
+/* Priyansh Agarwal*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> val(n);
+    vector<int> weight(n);
+    
+    for(int i = 0; i < n; i++){
+        cin >> weight[i];
+    }
+    
+    for(int i = 0; i < n; i++){
+        cin >> val[i];
+    }
+    
+    vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
+    // dp[i][j] = max value that can be attained from i elements,
+    // such that j weight is allowed to be used
+    
+    // base case
+    // dp[0][k] = 0 for every k because we cannot add any more value
+    // if 0 elements are left
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= x; j++){
+            // compute dp[i][j] here
+            int w = weight[i - 1];
+            int value = val[i - 1];
+            
+            // two choices
+            //  - pick up ith element
+            //  - skip ith element
+            int pick = (j >= w ? dp[i - 1][j - w] + value : 0);
+            int skip = dp[i - 1][j];
+            // transition
+            dp[i][j] = max(skip, pick); 
+        }
+    }
+    
+    // final subproblem
+    cout << dp[n][x] << endl;
+    return 0;
+}
---
+++ Solution Codes/7_book_shop_optimized_space.cpp
@@ -0,0 +1,49 @@
+/* Priyansh Agarwal*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> val(n);
+    vector<int> weight(n);
+    
+    for(int i = 0; i < n; i++){
+        cin >> weight[i];
+    }
+    
+    for(int i = 0; i < n; i++){
+        cin >> val[i];
+    }
+    
+    vector<int> prev(x + 1, 0);
+    // dp[i][j] = max value that can be attained from i elements,
+    // such that j weight is allowed to be used
+    
+    // base case
+    // dp[0][k] = 0 for every k because we cannot add any more value
+    // if 0 elements are left
+    
+    for(int i = 1; i <= n; i++){
+        vector<int> curr(x + 1);
+        for(int j = 0; j <= x; j++){
+            // compute dp[i][j] here
+            int w = weight[i - 1];
+            int value = val[i - 1];
+            
+            // two choices
+            //  - pick up ith element
+            //  - skip ith element
+            int pick = (j >= w ? prev[j - w] + value : 0);
+            int skip = prev[j];
+            // transition
+            curr[j] = max(skip, pick); 
+        }
+        prev = curr;
+    }
+    
+    // final subproblem
+    cout << prev[x] << endl;
+    return 0;
+}
